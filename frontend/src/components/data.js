68c4453d90939ad2b06aa551eeac90eb65f5a7d2
Nirmal Kumar Das <you@example.com>
2024-08-10 14:52:16 +0530

    Changed Commit

--- frontend/src/components/data.js
+++ frontend/src/components/data.js
@@ -1,4 +1,4 @@
-let movies = ['Kalki 2898 AD', 'Stree 2', 'Deadpool & Wolverine', 'Fly Me to the Moon', 'Bad Boys']
+let movies = ['Manjummel Boys', 'Dune 2', 'Brahmayugam', 'Leo', 'Blue Star']
 let slots = ['10:00 AM', '01:00 PM', '06:00 PM', "10:00 PM"]
 let seats = ['A1', 'A2', 'A3', 'A4', 'D1', 'D2']
 
